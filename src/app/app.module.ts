38df16c821be934a94de58962416f0eb454cde93
LeoGalvan <leonelgalvan619@gmail.com>
2023-11-06 18:55:22 -0600

    06-11-23 GMLL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MaterialModule } from './material/material.module';
 import { MenuComponent } from './menu/menu/menu.component';
 import { AgregarComponent } from './utl/agregar/agregar.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
